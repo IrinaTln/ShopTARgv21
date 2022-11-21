23ac3e76f7b3db7b21f8e4fcd472760a81c8f7c6
IrinaTln <ikosenkova@gmail.com>
2022-11-21 10:22:43 +0200

    ApplicationServices

---
+++ Shop/ShopTargv21.ApplicationServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopTargv21.ApplicationServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
