9777fdfa6cd9f2dd028767be744a8b132a320cb8
IrinaTln <ikosenkova@gmail.com>
2022-11-21 11:05:30 +0200

    SpaceshipModelAndController

---
+++ Shop/Shop/Controllers/SpaceshipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class SpeiceshipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
